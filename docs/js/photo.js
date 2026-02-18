1cf20ca764cfba5870882c7b1dfeeeef5fcdbe13
fxi <f@fxi.io>
2026-02-18 15:45:18 +0100

    fix: canvas style filter

--- docs/js/photo.js
+++ docs/js/photo.js
@@ -329,7 +329,7 @@ map.on("load", function () {
   startShake();
 
   canvasElement = mapDiv.querySelector("canvas");
-  canvasElement.style.filter = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='b' x='0' y='0'%3E%3CfeFlood x='4' y='4' height='2' width='2'/%3E%3CfeComposite width='10' height='10'/%3E%3CfeTile result='a'/%3E%3CfeComposite in='SourceGraphic' in2='a' operator='in'/%3E%3CfeMorphology operator='dilate' radius='1.3'/%3E%3C/filter%3E%3C/svg%3E#b")`;
+  // canvasElement.style.filter = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='b' x='0' y='0'%3E%3CfeFlood x='4' y='4' height='2' width='2'/%3E%3CfeComposite width='10' height='10'/%3E%3CfeTile result='a'/%3E%3CfeComposite in='SourceGraphic' in2='a' operator='in'/%3E%3CfeMorphology operator='dilate' radius='1.3'/%3E%3C/filter%3E%3C/svg%3E#b")`;
 });
 
 function updateFilmGrain() {
